88d0f4999f361625b76435a6f93ca5773eb53fb9
teler <teler@mail.ru>
2016-09-16 20:21:48 +0300

    Update installDIGITS.sh

--- installDIGITS.sh
+++ installDIGITS.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
-git clone  install-digits-4-on-AWS/installDIGITS.sh 
+#wget https://raw.githubusercontent.com/teler/install-digits-4-on-AWS/master/installDIGITS.sh
 #chmod +x installDIGITS.sh
 #./installDIGITS.sh
+
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install -y linux-image-extra-`uname -r`
+#Install NVIDIA drivers
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt-get install nvidia-352 nvidia-settings
+
+#Get CUDA and NVIDIA’s machine learning repos
+CUDA_REPO_PKG=cuda-repo-ubuntu1404_7.5-18_amd64.deb && 
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG && 
+sudo dpkg -i $CUDA_REPO_PKG
+ 
+ML_REPO_PKG=nvidia-machine-learning-repo_4.0-2_amd64.deb &&
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1404/x86_64/$ML_REPO_PKG &&
+sudo dpkg -i $ML_REPO_PKG
+
+#Install DIGITS
+sudo apt-get update
+sudo apt-get install digits
+
+#download bvlc_googlenet.caffemodel
+wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
+chmod u+x bvlc_googlenet.caffemodel
