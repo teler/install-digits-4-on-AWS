8f71ef536a3a67377d471ca99d2d6dcdaf4ef585
teler <teler@mail.ru>
2016-09-18 16:52:09 +0300

    New driver version

--- installDIGITS.sh
+++ installDIGITS.sh
@@ -9,12 +9,14 @@ sudo apt-get install -y linux-image-extra-`uname -r`
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
 sudo apt-get install nvidia-352 nvidia-settings
-
+#new driver version
+http://us.download.nvidia.com/XFree86/Linux-x86_64/367.44/NVIDIA-Linux-x86_64-367.44.run
+#NEW cuda
 #Get CUDA and NVIDIA’s machine learning repos
 CUDA_REPO_PKG=cuda-repo-ubuntu1404_7.5-18_amd64.deb && 
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG && 
 sudo dpkg -i $CUDA_REPO_PKG
- 
+ #NEW no repo
 ML_REPO_PKG=nvidia-machine-learning-repo_4.0-2_amd64.deb &&
 wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1404/x86_64/$ML_REPO_PKG &&
 sudo dpkg -i $ML_REPO_PKG
