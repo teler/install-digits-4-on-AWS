0335ff3e5add7baf20b503d7fe3fae5de0d0363a
teler <teler@mail.ru>
2016-09-18 18:30:21 +0300

    Update

--- installDIGITS.sh
+++ installDIGITS.sh
@@ -8,15 +8,14 @@ sudo apt-get install -y linux-image-extra-`uname -r`
 #Install NVIDIA drivers
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
-sudo apt-get install nvidia-352 nvidia-settings
-#new driver version
-http://us.download.nvidia.com/XFree86/Linux-x86_64/367.44/NVIDIA-Linux-x86_64-367.44.run
-#NEW cuda
+sudo apt-get install nvidia-361 nvidia-settings
+
+
 #Get CUDA and NVIDIA’s machine learning repos
 CUDA_REPO_PKG=cuda-repo-ubuntu1404_7.5-18_amd64.deb && 
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG && 
 sudo dpkg -i $CUDA_REPO_PKG
- #NEW no repo
+ 
 ML_REPO_PKG=nvidia-machine-learning-repo_4.0-2_amd64.deb &&
 wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1404/x86_64/$ML_REPO_PKG &&
 sudo dpkg -i $ML_REPO_PKG
@@ -25,6 +24,12 @@ sudo dpkg -i $ML_REPO_PKG
 sudo apt-get update
 sudo apt-get install digits
 
+sudo reboot
+
 #download bvlc_googlenet.caffemodel
 wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
 chmod u+x bvlc_googlenet.caffemodel
+
+ls /usr/share/digits/digits/jobs
+sudo stop nvidia-digits-server
+sudo start nvidia-digits-server
